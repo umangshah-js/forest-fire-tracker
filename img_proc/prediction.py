d8213f7f430e9b8d7ac705f656d9dcbd86a57f4f
Mohit Mehta <mehtamohit013@gmail.com>
2023-05-08 20:35:26 -0400

    Optimized Dask Storing centers in zarr format

--- img_proc/prediction.py
+++ img_proc/prediction.py
@@ -14,11 +14,13 @@ def prediction(npzfile_pth,
                contour_chunk = (256,256),
                 center_chunk = (5000,4),
                 eps = 1e-3,
-                k1 = 0.00001,
+                k1 = 0.001,
                 k2 = 10,
                 alpha = 5,
                 offset = 7,
-                save_path = "test.png",
+                dim=6155,
+                img_save_path = "test.png",
+                zarr_save_path = "centers.zarr",
                 f = open("prediction.log","a+")
             ):
     # countours_da  = da.from_array(npzfile['contours'], chunks=(512, 512))
@@ -40,33 +42,33 @@ def prediction(npzfile_pth,
 
     # countours = da.from_array(np.load(npzfile_pth)['contours'], chunks=contour_chunk)
     centers = da.from_array(np.load(npzfile_pth)['centers'], chunks=center_chunk)
-    dim = max(da.max(centers[:,1]), da.max(centers[:,0])) +1
-
 
     # Get the burning centers
     ind = (centers[:, 3]>=2) & (centers[:,3]<4) & (centers[:, 2]>=10)
     burning_centers = centers[ind,:]
-    heat = da.from_array(np.zeros((centers.shape[0], 1)),chunks=(center_chunk[0],1))
+    # heat = da.from_array(np.zeros((centers.shape[0], 1)),chunks=(center_chunk[0],1))
     
     # Computing Chunk sizes
     # heat.compute_chunk_sizes()
-    burning_centers.compute_chunk_sizes()
-    print(f"Number of burning trees: {burning_centers.shape[0]}", file=f)
+    # burning_centers.compute_chunk_sizes()
+    # print(f"Number of burning trees: {burning_centers.shape[0]}", file=f)
 
 
     # Get the heats for each center
-    for i in range(burning_centers.shape[0]):
-        norm = da.linalg.norm(centers[:,:2] - burning_centers[i, :2], axis=1, ord=2)[...,None]
-        heat += (k2/(norm+eps) + k1/(1+(da.exp(((norm/10000)+eps)))))*100
+    # for i in range(burning_centers.shape[0]):
+    #     norm = da.linalg.norm(centers[:,:2] - burning_centers[i, :2], axis=1, ord=2)[...,None]
+    #     heat += (k2/(norm+eps) + k1/(1+(da.exp(((norm/10000)+eps)))))*100
+    coord = centers[:,:2][...,None]
+    diff = coord - burning_centers[:, :2].T
+    norm = da.linalg.norm(diff, axis=1, ord=2)
+    norm = k2/(norm+eps) + k1/(1+(da.exp(((norm/10000)+eps))))
+    heat = da.sum(norm, axis=1)[...,None]
 
     # Setting the fireline
     # Using mean and std deviation to set the threshold
-    tmp = heat.copy()
-    # tmp.compute_chunk_sizes()
-
-    tmp[ind]=0
-    lim = tmp.mean()
-    lim_std = tmp.std()
+ 
+    lim = heat[~ind].mean()
+    lim_std = heat[~ind].std()
 
     # plt.hist(tmp,bins=100)
     # plt.xlabel("Heat")
@@ -95,26 +97,33 @@ def prediction(npzfile_pth,
     # Number of trees dead(Black): {centers[centers[:,3]==4].shape[0]}
     # """, file = f)
 
-    
-    img = np.full((int(dim), int(dim), 3), 255, dtype=np.uint8)
+    # Store the centers array in zarr format
+    centers.to_zarr(zarr_save_path, overwrite=True)
 
-    cv2.imwrite(save_path, draw_circle(img=img, centers=centers.compute(), states=states, thickness=-1) )
+    # For visualization and testing purposes only
+    # img = np.full((int(dim), int(dim), 3), 255, dtype=np.uint8)
+    # cv2.imwrite(img_save_path, draw_circle(img=img, centers=centers.compute(), states=states, thickness=-1) )
 
-    del img, centers, heat, burning_centers, ind, ind2, ind_heat, tmp
+    del  centers, heat, burning_centers, ind, ind2, ind_heat, thresh_max, thresh_min, lim, lim_std, coord, diff, norm
 
 
 if __name__ == "__main__":
  
-    client = Client(n_workers=2, threads_per_worker=2, memory_limit='4GB')
+    client = Client(n_workers=3, threads_per_worker=1, memory_limit='4GB')
     print(client)
     timestamps = 153
     f = open("prediction.log","w+")
 
-    for i in tqdm(range(1,timestamps)):
+    for i in tqdm(range(0,timestamps)):
         npzfile_pth = f"/home/mohit/NYU/Big_Data/Homework/forest-fire-tracker/data/timestamps/arr/{i}.npz"
         print(f"{'-'*20} For timestamp {i} {'-'*20}", file = f)
-        os.makedirs(f"../data/prediction/", exist_ok=True)
-        prediction(npzfile_pth, save_path=f"../data/prediction/{i}.png", f = f)
+        
+        img_save_path = f"/home/mohit/NYU/Big_Data/Homework/forest-fire-tracker/data/prediction/"
+        zarr_save_path = f"/home/mohit/NYU/Big_Data/Homework/forest-fire-tracker/data/zarr/"
+        os.makedirs(img_save_path, exist_ok=True)
+        os.makedirs(zarr_save_path, exist_ok=True)
+
+        prediction(npzfile_pth, img_save_path=f"{img_save_path}/{i}.png",zarr_save_path=f"{zarr_save_path}/{i}.zarr",f = f)
         print(f"Done with {i}.png", file = f)
         # break
 
