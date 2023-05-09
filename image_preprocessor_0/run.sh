a974a342c783fb39e3f23f668a5e2b7e20437625
Umang Shah <umangshah97@gmail.com>
2023-05-09 05:10:39 -0400

    kafka basic producer and consumer added

---
+++ image_preprocessor_0/run.sh
@@ -0,0 +1,2 @@
+python main.py > 1.out &
+python main.py > 2.out &
--- kafka_topic/setup.sh
+++ kafka_topic/setup.sh
@@ -2,5 +2,5 @@ cd kafka_2.13-3.4.0;
 bin/zookeeper-server-start.sh -daemon config/zookeeper.properties ;
 bin/kafka-server-start.sh -daemon config/server.properties ;
 wait
-bin/kafka-topics.sh --bootstrap-server localhost:9092 --describe --topic wfs-events || bin/kafka-topics.sh --create --topic image-preprocess-0 --bootstrap-server localhost:9092
+bin/kafka-topics.sh --create --topic image-preprocess-0 --bootstrap-server localhost:9092 --partitions 4 --if-not-exists
 
